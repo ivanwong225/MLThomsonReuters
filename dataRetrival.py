303acb73146cfbf4fae79af31bb43fc2aaf40b3e
Rohan Johar <rjohar@outlook.com>
2017-11-14 16:32:10 -0500

    Past Prices and Format

--- dataRetrival.py
+++ dataRetrival.py
@@ -1,12 +1,22 @@
 import json
-import requests 
+import requests
+import datetime
+from decimal import Decimal
 
 #Basic coindesk API hook
+#Need to convert pastPrices to 2-D Array of Time / Price
 
-def get_request(price):
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice/" + price + ".json")
+def getCurrentPrice():
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
     j_obj = json.loads(response.text)
-    print("As of " + j_obj["time"]["updated"] + ", the price of a Bitcoin in " + price + " is: " + j_obj["bpi"]["USD"]["rate"])
+    x = j_obj["bpi"]["USD"]["rate"]
+    return Decimal(x.replace(",", ""))
 
-
-get_request("USD")
+def getPastPrices(months): 
+    today = datetime.date.today()
+    startDate = today - datetime.timedelta(months*365/12)
+    endDate = today - datetime.timedelta(1)
+    response = requests.get("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + str(startDate) + "&end=" + str(endDate))
+    j_obj = json.loads(response.text)
+    pastPrices = j_obj["bpi"]
+    print(pastPrices) #Need to convert pastPrices to 2-D Array of Time / Price
