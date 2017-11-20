585386a66297efe94d81bddc3460dec71f3db044
Rohan Johar <rjohar@outlook.com>
2017-11-19 23:17:56 -0500

    Linear Regression

--- dataRetrival.py
+++ dataRetrival.py
@@ -40,8 +40,19 @@ data = np.asarray(data)
 n = data.shape[0]
 X,y = data[:, 0, np.newaxis], data[:, 1, np.newaxis]
 X = X.astype(int)
-y = y/1000
 
+#Linear
+import scipy.stats as spcs
+values = spcs.mstats.linregress(X, y)
+slope, intercept = values[0], values[1]
+linearFunc = lambda x: slope * x + intercept
+y2 = list(map(linearFunc, X))
+plt.plot(X, y, 'ro')
+plt.plot(X, y2, 'r--')
+plt.xlim(0, len(X))
+plt.show()
+
+#Older Stuff
 # print(X)
 # x = []
 # y = []
@@ -58,91 +69,91 @@ y = y/1000
 #
 # plt.plot(x, y, 'yo', x, z_n(x), '--')
 # plt.show()
-def add_column(X):
-    assert len(X.shape) == 2 and X.shape[1] == 1
+#def add_column(X):
+#   assert len(X.shape) == 2 and X.shape[1] == 1
 
     # raise NotImplementedError("Insert a column of ones to right side of the matrix")
-    return np.insert(X, 0, 1, axis=1)
+#    return np.insert(X, 0, 1, axis=1)
 
-def predict(X, theta):
-    """ Computes h(x; theta) """
-    assert len(X.shape) == 2 and X.shape[1] == 1
-    assert theta.shape == (2, 1)
+#def predict(X, theta):
+#    """ Computes h(x; theta) """
+#    assert len(X.shape) == 2 and X.shape[1] == 1
+#    assert theta.shape == (2, 1)
 
-    X_prime = add_column(X)
-    pred = X_prime @ theta
+#    X_prime = add_column(X)
+#    pred = X_prime @ theta
     # raise NotImplementedError("Compute the regression predictions")
-    return pred
+#    return pred
 
-def loss(X, y, theta):
-    assert X.shape == (n, 1)
-    assert y.shape == (n, 1)
-    assert theta.shape == (2, 1)
+#def loss(X, y, theta):
+#    assert X.shape == (n, 1)
+#    assert y.shape == (n, 1)
+#    assert theta.shape == (2, 1)
 
-    X_prime = add_column(X)
-    assert X_prime.shape == (n, 2)
+#    X_prime = add_column(X)
+#    assert X_prime.shape == (n, 2)
 
     # raise NotImplementedError("Compute the model loss; use the predict() function")
-    loss = ((predict(X, theta) - y)**2).mean()/2
-    return loss
+ #   loss = ((predict(X, theta) - y)**2).mean()/2
+ #   return loss
 
-theta_init = np.zeros((2, 1))
-print(loss(X, y, theta_init))
+#theta_init = np.zeros((2, 1))
+#print(loss(X, y, theta_init))
 
-import scipy.optimize
-from functools import partial
+#import scipy.optimize
+#from functools import partial
 
-def loss_gradient(X, y, theta):
-    X_prime = add_column(X)
-    loss_grad = ((predict(X, theta) - y)*X_prime).mean(axis=0)[:, np.newaxis]
+#def loss_gradient(X, y, theta):
+#    X_prime = add_column(X)
+#    loss_grad = ((predict(X, theta) - y)*X_prime).mean(axis=0)[:, np.newaxis]
 #     raise NotImplementedError("Compute the model loss gradient; "
 #                               "use the predict() function; "
 #                               "this also must be vectorized!")
-    return loss_grad
+#   return loss_grad
 
-assert loss_gradient(X, y, theta_init).shape == (2, 1)
+#assert loss_gradient(X, y, theta_init).shape == (2, 1)
 
-def run_gd(loss, loss_gradient, X, y, theta_init, lr=.0001, n_iter=1500):
-    theta_current = theta_init.copy()
-    loss_values = []
-    theta_values = []
+#def run_gd(loss, loss_gradient, X, y, theta_init, lr=.0001, n_iter=1500):
+#    theta_current = theta_init.copy()
+#    loss_values = []
+#    theta_values = []
 
-    for i in range(n_iter):
-        loss_value = loss(X, y, theta_current)
-        theta_current = theta_current - lr*loss_gradient(X, y, theta_current)
-        loss_values.append(loss_value)
-        theta_values.append(theta_current)
+#    for i in range(n_iter):
+#        loss_value = loss(X, y, theta_current)
+#        theta_current = theta_current - lr*loss_gradient(X, y, theta_current)
+#        loss_values.append(loss_value)
+#        theta_values.append(theta_current)
 
-    return theta_current, loss_values, theta_values
+ #   return theta_current, loss_values, theta_values
 
-result = run_gd(loss, loss_gradient, X, y, theta_init)
-theta_est, loss_values, theta_values = result
+#result = run_gd(loss, loss_gradient, X, y, theta_init)
+#theta_est, loss_values, theta_values = result
 
-print('estimated theta value', theta_est.ravel())
-print('resulting loss', loss(X, y, theta_est))
-plt.ylabel('loss')
-plt.xlabel('iter_i')
-plt.plot(loss_values)
-plt.show()
+#print('estimated theta value', theta_est.ravel())
+#print('resulting loss', loss(X, y, theta_est))
+#plt.ylabel('loss')
+#plt.xlabel('iter_i')
+#plt.plot(loss_values)
+#plt.show()
 
-plt.ylabel('log(loss)')
-plt.xlabel('iter_i')
-plt.semilogy(loss_values)
-plt.show()
-plt.scatter(X, y, marker='x', color='r', alpha=0.5)
-plt.show()
+#plt.ylabel('log(loss)')
+#plt.xlabel('iter_i')
+#plt.semilogy(loss_values)
+#plt.show()
+#plt.scatter(X, y, marker='x', color='r', alpha=0.5)
+#plt.show()
 
-plt.scatter(X, y, marker='x', color='r', alpha=0.5)
-x_start, x_end = 0, 100
-plt.xlim(x_start, x_end)
-plt.ylim(0, 9)
-X_test = np.array([[0], [100]])
-y_test = predict(X_test, theta_est)
-plt.plot(X_test, y_test)
+#plt.scatter(X, y, marker='x', color='r', alpha=0.5)
+#x_start, x_end = 0, 100
+#plt.xlim(x_start, x_end)
+#plt.ylim(0, 9)
+#X_test = np.array([[0], [100]])
+#y_test = predict(X_test, theta_est)
+#plt.plot(X_test, y_test)
 
-plt.show()
+p#lt.show()
 
 #Test
-print(predict(np.array([[80], [120]]), theta_est))
+#print(predict(np.array([[80], [120]]), theta_est))
 
 
