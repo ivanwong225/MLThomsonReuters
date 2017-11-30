4e4e632015ea83a53776888ada35c555d1b81fca
Rohan Johar <rjohar@outlook.com>
2017-11-30 12:54:31 -0500

    Rename Testing to Anomaly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import dataRetrival
 import webScraper
 import excel
-import testing
+import anomaly as an
 
 def main(months, var):
 	currentPrice = dataRetrival.getCurrentPrice()
@@ -11,8 +11,8 @@ def main(months, var):
 	linearVars = dataRetrival.linearRegression(pastPricesX, pastPricesY)
 	degree = dataRetrival.crossValidationDegree(pastPricesX, pastPricesY)
 	polyVars = dataRetrival.polynomialRegression(pastPricesX, pastPricesY, degree)
-	anomaly = testing.checkAnomaly(linearVars, polyVars, len(pastPricesX) + 1, currentPrice, var) #low
-	headline = testing.createHeadline(anomaly, currentPrice, previousPrice)
+	anomaly = an.checkAnomaly(linearVars, polyVars, len(pastPricesX) + 1, currentPrice, var) #low
+	headline = an.createHeadline(anomaly, currentPrice, previousPrice)
 	extraInfo = webScraper.extraBTCInfo(2)[0]
 	pChange = int(((currentPrice - previousPrice) / (previousPrice)) * 100)
 	#excel.WB(currentPrice, anomaly, headline, str(pChange) + "%", extraInfo)
